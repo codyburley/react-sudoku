b767d2c18f5e1e3a94b6ccebb1a919897748c83b
Cody Burley <codyburley@gmail.com>
2022-12-06 16:12:16 -0500

    added grid spacing

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Game.scss';
+import { CONSTANT } from '../../constant';
 
 const Game = () => {
   const renderCells = () => {
+    let index = 0;
     let cells = [];
-    for (let i = 0; i <= 81; i++) {
-      cells.push(<div className='game__cell' key={i}></div>)
+    for (let i = 0; i < 81; i++) {
+      let row = Math.floor(i / CONSTANT.GRID_SIZE);
+      let col = i % CONSTANT.GRID_SIZE;
+      if (row === 2 || row === 5) cells.push(<div className='game__cell game__cell--bottom' key={i}></div>);
+      else if (col === 2 || col === 5) cells.push(<div className='game__cell game__cell--right' key={i}></div>);
+      else cells.push(<div className='game__cell' key={i}></div>)
+      index++;
     }
     return cells;
   }
